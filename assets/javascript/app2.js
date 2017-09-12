2173b3a9bcc3177634cdc8d5813a0924ca3210df
AJama2030 <Ajama2030@gmail.com>
2017-09-11 22:39:22 -0700

    Recoverd file again

---
+++ assets/javascript/app2.js
@@ -0,0 +1,29 @@
+
+  var config = {
+    apiKey: "AIzaSyDe27JaRxwKau9DdMBa0wLQXaD7ivPqOPs",
+    authDomain: "positive-portfolio-factor.firebaseapp.com",
+    databaseURL: "https://positive-portfolio-factor.firebaseio.com",
+    projectId: "positive-portfolio-factor",
+    storageBucket: "positive-portfolio-factor.appspot.com",
+    messagingSenderId: "223064031382"
+  };
+  firebase.initializeApp(config);
+
+
+  var database = firebase.database();
+   database.ref().on('child_added', function(childSnapshot) {
+
+  var userName = childSnapshot.val().userName;
+  var projectName = childSnapshot.val().projectName;
+  var projectLink = childSnapshot.val().projectLink;
+  
+  console.log("yaaa");
+
+  $('#projectTable').append("<tr><td><a href='./project-details.html'>" + userName + "</td><td>"  + projectName + "</a></td><td>"
+    + projectLink+ "</td></tr>");
+
+
+// var urlTwo = "./project-details.html";
+//  window.location = urlTwo;
+
+});
